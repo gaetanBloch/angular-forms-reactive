231ce61b04e360d3abd01f0a31450f7bf8947fc7
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-12 21:38:53 +0200

    Set up the Reactive Form Group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
