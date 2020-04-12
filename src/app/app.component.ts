c7a227f0ba27f0dc7acd1a75a1c18f12c91a2aaa
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-12 22:14:16 +0200

    Added validation on username and email controls of the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +13,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
-      username: new FormControl(null),
-      email: new FormControl(null),
+      username: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       gender: new FormControl(this.defaultGender)
     });
   }
